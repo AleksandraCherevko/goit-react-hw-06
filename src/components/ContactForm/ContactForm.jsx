5dc48d9cdb5cc9f5dfb407d0b5d1d2e30fc09a39
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-07-31 21:29:10 +0200

    add scc

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,12 +1,15 @@
+import css from "./ContactForm.module.css";
 
 export default function ContactForm() {
   return (
     <div>
-      <p>Name</p>
-      <input type="text" />
-      <p>Number</p>
-      <input type="text" />
-      <button type="button">Add Contact</button>
+      <p className={css.inputTitle}>Name</p>
+      <input className={css.input} type="text" />
+      <p className={css.inputTitle}>Number</p>
+      <input className={css.input} type="text" />
+      <button className={css.addContactBtn} type="button">
+        Add Contact
+      </button>
     </div>
   );
 }
--- src/components/SearchBox/SeacrhBox.jsx
+++ src/components/SearchBox/SeacrhBox.jsx
@@ -1,9 +1,10 @@
+import css from "./SearchBox.module.css";
 
 export default function SearchBox() {
   return (
-    <div>
-      <p>Find contacts by name</p>
-      <input type="text" />
+    <div className={css.searchBox}>
+      <p className={css.seacrhBoxTitle}>Find contacts by name</p>
+      <input className={css.seacrhInput} type="text" />
     </div>
   );
 }
